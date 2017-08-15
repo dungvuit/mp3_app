acc19dad5b1ec91bab1c8b6c5ce47b009e329c9d
dungvuit <dungvuit@gmail.com>
2017-08-15 17:32:57 +0700

    Finish like dislike song

--- app/assets/javascripts/dislike.js
+++ app/assets/javascripts/dislike.js
@@ -1,8 +1,9 @@
 jQuery(document).ready(function($){
   document.addEventListener("turbolinks:load", function() {
-    $("#dislike").on('click', function() {
+    $(document).on('click',"#dislike", function() {
       var id;
       id = $(this).attr('data_id');
+      console.log($(this).attr('data_id'))
       $.ajax ( {
         url: '/likes/' + id,
         type: 'DELETE',
